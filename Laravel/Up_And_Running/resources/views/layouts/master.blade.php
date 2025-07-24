dcada9b75753c4383ddd4fb863e86665bc4c3c85
Abbas <mr_yavar@outlook.com>
2025-07-24 12:54:51 +0330

    .

--- Laravel/Up_And_Running/resources/views/layouts/master.blade.php
+++ Laravel/Up_And_Running/resources/views/layouts/master.blade.php
@@ -6,10 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- yield,section/show is called when we want add content later  -->
     <title>@yield('title','Home')</title>
+    <style>
+
+
+        </style>
 </head>
 <body>
     <div class="container">@yield('container')</div>
-
+    <div>
+        @include('modules.option',['option'=>5])
+        @each('modules.option',[['option'=>time()],['option'=>time()],['option'=>time()]],'option' )
+    </div>
     @section('footerScript')
         <footer>default</footer>
     @show
